3eec5795349224b7950fd38c164c29e9f29cbb25
Suraj Prajapati <43133450+SurajPrajapati8424@users.noreply.github.com>
2019-10-24 18:16:38 +0530

    Add files via upload

---
+++ shree classes/js/submit.js
@@ -0,0 +1,10 @@
+// JavaScript DocumentŚ
+function sendData()
+{
+	var e= document.getElementById("email").value;
+	var n= document.getElementById("name").value;
+	var s= document.getElementById("subject").value;
+	var m= document.getElementById("message").value;
+	alert("Thanks for your Feedback "+n);
+	
+}
